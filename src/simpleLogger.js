cbfbfb0d1a8e4baeefb934743b90b6519f7f675a
Vadorequest <ambroise.dhenain@gmail.com>
2019-11-15 23:12:56 +0100

    Update deps (#6)
    * Update deps
    
    * Allow to provide UNLY_SIMPLE_LOGGER_ENV that takes precedence over NODE_ENV
    
    * Remove node 6 support
    
    * Misc

--- src/simpleLogger.js
+++ src/simpleLogger.js
@@ -5,7 +5,6 @@ const {
   combine, timestamp, printf, splat, colorize,
 } = format;
 
-
 export const myFormat = printf((info) => {
   let meta = '';
   if (info.meta) {
@@ -14,19 +13,22 @@ export const myFormat = printf((info) => {
       'number',
     ].includes(typeof info.meta) ? `[${info.meta}]` : JSON.stringify(info.meta, null, 2);
   }
+
   return `${info.timestamp} [${info.label}] ${info.level}: ${meta} ${info.message}`; // Display complex type as json
 });
 
 /**
  * Create a logger based on many default options, only allow customisation of a few of them
  * see https://github.com/winstonjs/winston
+ *
  * @param label
  * @param level
  * @returns {winston.Logger}
  */
 export const createLogger = ({ label, defaultLevel }) => {
-  const isProd = process.env.NODE_ENV === 'production' ? 'error' : 'debug';
+  const isProd = (process.env.UNLY_SIMPLE_LOGGER_ENV || process.env.NODE_ENV) === 'production' ? 'error' : 'debug';
   const level = defaultLevel || isProd;
+
   return winston.createLogger({
     level,
     format: combine(colorize(), format.label({ label }), timestamp(), splat(), myFormat),
@@ -39,6 +41,7 @@ export const createLogger = ({ label, defaultLevel }) => {
 /**
  * Info log request origin with a default Logger and default options
  * see https://github.com/winstonjs/winston
+ *
  * @param req
  * @param label
  */
